0a5ce1e7baac9bc7c59ccf554b6686bba90aa2c4
nikhilchapain81-cell <nikhilchapain81@gmail.com>
2026-05-12 18:00:32 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.withdraw-btn').addEventListener('click', function() {
-    alert("Withdrawal request sent! Checking your UrbanDrip balance...");
-});
-
-console.log("UrbanDrip Dashboard Loaded Successfully!");
